5a9f3ff9e3701c9007ab29ec1ca1909f809cb572
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-09-12 12:02:34 +0700

    Penghapusan file double

--- dart_application_2/bin/dart_application_2.dart
+++
@@ -1,9 +0,0 @@
-// void main(List<String> arguments) {
-//   print('Hello world: ${dart_application_2.calculate()}!');
-// }
-
-void main() {
-  for(int i = 20; i > 10; i--) {
-    print('Nama saya Fulan, Sekarang berumur ${i - 2}');
-  }
-}
