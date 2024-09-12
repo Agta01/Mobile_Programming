3c4ccd24682aa88c8de8c06c3e551429e8ff7abf
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-09-12 11:54:04 +0700

    Pengerjaan Jobsheet 2

---
+++ dart_application_2/bin/dart_application_2.dart
@@ -0,0 +1,9 @@
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_application_2.calculate()}!');
+// }
+
+void main() {
+  for(int i = 20; i > 10; i--) {
+    print('Nama saya Fulan, Sekarang berumur ${i - 2}');
+  }
+}
