9db9cd11f76831379523e1b8718d3bc363dfe42a
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-09-12 11:40:53 +0700

    Pengerjaan Jobsheet

---
+++ Pertemuan3/Praktikum3.dart
@@ -0,0 +1,8 @@
+void main() {
+    for (Index = 10; indes < 27; index) {
+        print(Index);
+        If (Index == 21) break;
+        Else If (index > 1 || index < 7) continue;
+        print(index);
+    }
+}
